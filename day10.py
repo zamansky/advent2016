2421f900394d9dc9c0414cffa49ad4e725926984
Mike Zamansky <zamansky@gmail.com>
2016-12-11 20:13:09 -0500

    Z

--- day10.py
+++ day10.py
@@ -2,6 +2,7 @@ import re
 
 input=open("day10.dat").readlines()
 instructions=[]
+outputs={}
 robots={}
 for line in input:
     line = line.rstrip("\n")
@@ -18,16 +19,37 @@ for line in input:
         m=re.match(matcho,line)
         instructions.append((m.group(1),m.group(2),m.group(3),m.group(4),m.group(5)))
                     
-            
-for inst in instructions:
-    robots.setdefault(robot,{})
-    robots[robot].setdefault("chips",[])
 
-    if inst[0] in robots and len(robots[inst[0]]["chips"]) < 2:
-        continue
-    else:
-        # do the move
-        # we're done when we find out compares 61 and 17
-        pass
-
-        
+while True:
+    
+    for inst in instructions:
+        if inst[0] in robots and len(robots[inst[0]]["chips"])  <2:
+            pass
+        else:
+            # do the move
+            # we're done when we find out compares 61 and 17
+            try:
+                chips = robots[inst[0]]['chips']
+            except:
+                continue
+            small = min(chips)
+            big = max(chips)
+            robots[inst[0]]["chips"].remove(small)
+            robots[inst[0]]["chips"].remove(big)
+            print(small,big)
+            if small==17 and big==61:
+                print("DONE:", inst[0])
+                sys.exit(0)
+            if inst[1]=="bot":
+                robots.setdefault(inst[2],{})
+                robots[inst[2]].setdefault('chips',[])
+                robots[inst[2]]['chips'].append(small)
+            else:
+                outputs[inst[2]]=small
+            if inst[3]=="bot":
+                robots.setdefault(inst[4],{})
+                robots[inst[4]].setdefault('chips',[])
+                robots[inst[4]]['chips'].append(big)
+            else:
+                outputs[inst[4]]=small
+    print(outputs)
