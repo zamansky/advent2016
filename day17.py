368f079cfc0db4f2c7e0aa0e03f426eeb52b7570
Mike Zamansky <zamansky@gmail.com>
2016-12-17 20:57:46 -0500

    almost done for the night on 17

--- day17.py
+++ day17.py
@@ -1,6 +1,7 @@
 import hashlib
 
-oinput="hijkl"
+#input="hijkl"
+input="edjrjqaa"
 dstring = "UDLR"
 
 dirs=[(0,-1),(0,1),(-1,0),(1,0)]
@@ -17,16 +18,31 @@ def make_cells(tuple,sofar,input):
     h = hashlib.md5(s).hexdigest()[0:4]
     newcells = [ ((x+dirs[i][0],y+dirs[i][1]),sofar+dstring[i])
                  for i in range(4) if h[i] in opens
-                 and x+dirs[i][0]>=0 and y+dirs[i][1]>=0 ]
+                 and x+dirs[i][0]>=0 and y+dirs[i][1]>=0
+                 and (x+dirs[i][0],y+dirs[i][1],sofar+dstring[i] not in visited)]
     return newcells
 
+dones=[]
 
-def search(loc,dest,h):
+def search(loc,dest,input):
     q=[]
-    # current, prev, hash, prevdir
-    q.append((loc,dest,h,""))
+    # current, sofar
+    q.append((loc,""))
     while q:
-        pass
+        #print(q)
+        (current,sofar) = q[0]
+        visited[(current,sofar)]=sofar
+        q=q[1:]
 
+        if (current==dest):
+            dones.append(len(sofar))
+            print(len(sofar))
+            continue
+        
+        nexts = make_cells(current,sofar,input)
+        q=q+nexts
+        
 
     
+search((0,0),(3,3),input)
+print(max(dones))
