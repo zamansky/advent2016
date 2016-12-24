68c513aa06f724a24dd75565b62b176f74a77fc2
Mike Zamansky <zamansky@gmail.com>
2016-12-24 07:44:05 -0500

    added 24 dat

--- day22.py
+++ day22.py
@@ -10,6 +10,15 @@ for l in open("day22.dat").readlines():
     m = re.match(expr,l)
     t = [x for x in m.groups()]
     
-    drives.append({'x': t[0],'y':t[1],'size':t[2],'u':t[3],'a':t[4]})
+    drives.append({'x': int(t[0]),'y':int(t[1]),'size':int(t[2]),'u':int(t[3]),'a':int(t[4])})
 
-print( drives[0:10])
+
+byuse = sorted(drives,key=lambda x: x['u'],reverse=True)
+byavail = sorted(drives,key=lambda x: x['a'],reverse=True)
+print( byuse[0:10])
+print( byavail[0:10])
+
+for item in byuse:
+    fits = [x for x in byavail if x['a'] >= item['u']]
+    print(fits)
+    print(len(fits))
